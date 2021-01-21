f6c200c730191096a477f9a3b4df5aa5728dfa54
Ritesh Kumar <ritesh@emerald.pink>
2021-01-21 21:51:00 +0800

    Buggy Commit

--- lab1/L1/ex2/ex2.c
+++ lab1/L1/ex2/ex2.c
@@ -1,8 +1,8 @@
 /*************************************
 * Lab 1 Exercise 1
-* Name:
-* Matric No:
-* Lab Group:
+* Name: Ritesh Kumar
+* Matric No: A0201829H
+* Lab Group: 12
 *************************************/
 
 #include <stdio.h>
@@ -26,17 +26,19 @@ void destroyList(node*);
 int main()
 {
     node* myList = NULL;    //Empty List
-    int position, input;
+    int position, newValue, numberOfCopies; // input values
 
-    //Fill in code for input and processing
-
+	//Fill in code for input and processing
+	while(scanf("%i %i %i", &position, &newValue, &numberOfCopies) == 1 ) {
+		printf("testing");
+		myList = insertAt(myList, position, numberOfCopies, newValue);
+	}
     //Output code coded for you
     printf("My List:\n");
     printList(myList);
-    
+   return 0; 
     destroyList(myList);
     myList = NULL;
-
     
     printf("My List After Destroy:\n");
     printList(myList);
@@ -47,14 +49,43 @@ int main()
 //Actual Function Implementations
 node* insertAt(node* head, int position, int copies, int newValue)
 {
-    //Fill in your code here
-    return NULL;    //change this!
+	// creates linked list with that many copies: 
+    node* firstPtr = (node*) malloc(sizeof(node));
+	node* currentNode = firstPtr;
+	while(copies > 0) {
+		node* nextNodePtr = (node*) malloc(sizeof(node));
+		nextNodePtr->data = newValue;
+		nextNodePtr->next = NULL;
+		currentNode->data = newValue; 
+		currentNode->next = nextNodePtr;
+		currentNode = nextNodePtr;
+		copies--;
+	}
+
+	if(head == NULL) { 
+		return firstPtr;
+	} else {
+	    // finds insertion point and joining point w.r.t original linked list:
+		node* insertPosition = head;
+		while(position > 0) { // shift pointer to idx x - 1
+			if(head->next == NULL) break;
+			insertPosition = head->next;
+			position--;
+		}
+		node* joinPosition = insertPosition->next;
+		// set pointers appropriately:
+		insertPosition->next = firstPtr;
+		currentNode->next = joinPosition;	
+		//Fill in your code here
+		return head;    //change this!
+	}
 }
  
 void printList(node* head)
 //Purpose: Print out the linked list content
 //Assumption: the list is properly null terminated
 {
+	printf("printing list");
     //This function has been implemented for you
     node* ptr = head;
 
@@ -67,7 +98,13 @@ void printList(node* head)
 
 void destroyList(node* head)
 {
-     
     //Fill in your code here
     //You can use the same implementation as in exercise 1
+	if(head->next == NULL) {
+		free(head);
+	} else {
+		node* sublist = head->next;
+		free(head);
+		destroyList(sublist);
+	}
 }
