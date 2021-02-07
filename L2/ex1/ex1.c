d2f01e21e9557e7df5e94b81f2d5d5fb51c9a91c
Samuel Fang <samuelfangjw@gmail.com>
2021-02-07 15:32:55 +0800

    Add lab 2 files

---
+++ L2/ex1/ex1.c
@@ -0,0 +1,32 @@
+/*************************************
+* Lab 2 Exercise 1
+* Name:
+* Student Id: A????????
+* Lab Group: B??
+*************************************
+Note: Duplicate the above and fill in 
+for the 2nd member if  you are on a team
+--------------------------------------
+Warning: Make sure your code works on
+compute cluster node (Linux on x86)
+*************************************/
+
+#include <stdio.h>
+#include <sys/types.h>   
+#include <sys/stat.h>
+#include <unistd.h>     //for fork()
+#include <sys/wait.h>   //for wait()
+
+int main()
+{
+    int nChild;
+
+    //Read the number of child
+    scanf("%d", &nChild);
+
+    //Spawn child processes
+
+    //Wait on child processes in order
+     
+    return 0;
+}
---
+++ L2/ex2/ex2.c
@@ -0,0 +1,136 @@
+/*************************************
+* Lab 2 Exercise 2
+* Name:
+* Student Id: A????????
+* Lab Group: B??
+*************************************
+Note: Duplicate the above and fill in 
+for the 2nd member if  you are on a team
+Note2: Exercise 3 is a superset of exercise 2
+Please copy over your code to ex3.c and continue
+Submit ONLY ex3.c (even if you cannot finish ex3)
+--------------------------------------
+Warning: Make sure your code works on
+compute cluster node (Linux on x86)
+*************************************/
+
+#include <stdio.h>
+#include <fcntl.h>      //For stat()
+#include <sys/types.h>   
+#include <sys/stat.h>
+//#include <sys/wait.h>   //for waitpid()
+#include <unistd.h>     //for fork(), wait()
+#include <string.h>     //for string comparison etc
+#include <stdlib.h>     //for malloc()
+
+
+char** split( char* input, char* delimiter, int maxTokenNum, int* readTokenNum )
+//Assumptions:
+//  - the input line is a string (i.e. with NULL character at the end)
+//  - the delimiter is a string of possible delimiters, each delimiter is single chracter
+//Behavior:
+//  - Split up to and include maxTokenNum from the input string
+//Return: Tokenized substrings as array of strings
+//        readTokenNum stores the total number of tokens
+//Note:
+//  - Must use the freeTokenArray to free memory after use!
+{
+    char** tokenStrArr;
+    char* tStart;   //start of token
+    int i, strSize;      
+
+    //allocate token array, each element is a char*
+    tokenStrArr = (char**) malloc(sizeof(char*) * maxTokenNum );
+
+    //Nullify all entries
+    for (i = 0; i < maxTokenNum; i++){
+        tokenStrArr[i] = NULL;
+    }
+
+    //Look for next token as separated by the delimiter
+    tStart = strtok(input, delimiter); 
+
+    i = 0;
+    while (i < maxTokenNum && tStart){
+
+        strSize = strlen(tStart);
+
+        //Allocate space for token string. +1 for null terminator
+        tokenStrArr[i] = (char*) malloc(sizeof(char) * (strSize + 1) );
+
+        strcpy(tokenStrArr[i], tStart);    
+
+        i++;
+        tStart = strtok(NULL, delimiter); 
+    }
+
+    //return number of token read
+    *readTokenNum = i;
+
+    return tokenStrArr;
+}
+
+void freeTokenArray(char** strArr, int size)
+{
+    int i;
+
+    //Free each string first
+    for (i = 0; i < size; i++){
+        if (strArr[i])
+            free( strArr[i] );
+        strArr[i] = NULL;
+    }
+
+    //Free the entire array
+    free( strArr );
+
+    //Note: Caller still need to set the strArr parameter to NULL
+    //      afterwards
+}
+
+
+int main()
+{
+    char **cmdLineArgs;
+    char path[20] = ".";  //default search path
+    char userInput[121];
+
+    int tokenNum;
+
+    //read user input
+    printf("YWIMC > ");
+
+    //read up to 120 characters from user input
+    fgets(userInput, 120, stdin);
+
+    //split is a helper function to "tokenize" the input
+    //see "stringTokenizer.c" program for more sample usage 
+    cmdLineArgs = split( userInput, " \n", 7, &tokenNum );
+
+    //At this point you have the user input split neatly into token in cmdLineArg[]
+
+    while ( strcmp( cmdLineArgs[0], "quit") != 0 ){
+
+        //Figure out which command the user want and implement below
+
+
+        //Prepare for next round input
+
+        //Clean up the token array as it is dynamically allocated
+        freeTokenArray(cmdLineArgs, tokenNum);
+
+        printf("YWIMC > ");
+        fgets(userInput, 120, stdin);
+        cmdLineArgs = split( userInput, " \n", 7, &tokenNum );
+    }
+    
+
+    printf("Goodbye!\n");
+
+    //Clean up the token array as it is dynamically allocated
+    freeTokenArray(cmdLineArgs, tokenNum);
+
+
+    return 0;
+
+}
