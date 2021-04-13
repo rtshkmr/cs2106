0e3d39c8602a485b384e6abc52a0d99c8d796e83
Ritesh Kumar <ritesh@emerald.pink>
2021-04-13 22:22:52 +0800

    Complete Ex3. Task 3 Buddy DeAllocation
    This suffers from some memory leak somewhere. Valgrind claims it's to a pointer that already exists so I'm going to leave the mem leak bug as a lower-priority TODO.

--- L4/L4/ex3/ex3.c
+++ L4/L4/ex3/ex3.c
@@ -1,9 +1,9 @@
 /*************************************
-* Lab 4 Exercise 3
-* Name:
-* Student Id: A????????
-* Lab Group: B??
-*************************************
+ * Lab 4 Exercise 3
+ * Name:
+ * Student Id: A????????
+ * Lab Group: B??
+ *************************************
 Note: Duplicate the above and fill in 
 for the 2nd member if  you are on a team
 */
@@ -29,10 +29,10 @@ static heapMetaInfo hmi;
 #define powOf2(E) (1 << E)
 
 unsigned int log2Ceiling( unsigned int N )
-/**********************************************************
- * Find the smallest S, such that 2^S >= N
- * S is returned
- *********************************************************/
+    /**********************************************************
+     * Find the smallest S, such that 2^S >= N
+     * S is returned
+     *********************************************************/
     // TODO: use this for allocation algo
 {
     unsigned int s = 0, pOf2 = 1;
@@ -47,10 +47,10 @@ unsigned int log2Ceiling( unsigned int N )
 
 
 unsigned int log2Floor( unsigned int N )
-/**********************************************************
- * Find the largest S, such that 2^S <= N
- * S is returned
- *********************************************************/
+    /**********************************************************
+     * Find the largest S, such that 2^S <= N
+     * S is returned
+     *********************************************************/
 {
     unsigned int s = 0, pOf2 = 1;
 
@@ -63,9 +63,9 @@ unsigned int log2Floor( unsigned int N )
 }
 
 unsigned int buddyOf( unsigned int addr, unsigned int lvl )
-/**********************************************************
- * Return the buddy address of address (addr) at level (lvl)
- *********************************************************/
+    /**********************************************************
+     * Return the buddy address of address (addr) at level (lvl)
+     *********************************************************/
 {
     unsigned int mask = 0xFFFFFFFF << lvl;
     unsigned int buddyBit = 0x0001 << lvl;
@@ -74,59 +74,59 @@ unsigned int buddyOf( unsigned int addr, unsigned int lvl )
 }
 
 partInfo* buildPartitionInfo(unsigned int offset)
-/**********************************************************
- * Allocate a new partInfo structure and initialize the fields
- *********************************************************/
+    /**********************************************************
+     * Allocate a new partInfo structure and initialize the fields
+     *********************************************************/
 {
     partInfo *piPtr;
 
     piPtr = (partInfo*) malloc(sizeof(partInfo));
 
     piPtr->offset = offset;
-	piPtr->nextPart = NULL;
+    piPtr->nextPart = NULL;
 
     //Buddy system's partition size is implicit
-	//piPtr->size = size;
+    //piPtr->size = size;
 
     //All available partition in buddy system is implicitly free
-	//piPtr->status = FREE;
+    //piPtr->status = FREE;
 
     return piPtr;
 }
 
 void printPartitionList(partInfo* piPtr)
-/**********************************************************
- * Print a partition linked list
- *********************************************************/
+    /**********************************************************
+     * Print a partition linked list
+     *********************************************************/
 {
-	partInfo* current;
+    partInfo* current;
     int count = 1;
-	
-	for ( current = piPtr; current != NULL; 
-		current = current->nextPart){
+
+    for ( current = piPtr; current != NULL; 
+            current = current->nextPart){
         if (count % 8 == 0){
             printf("\t");
         }
-		printf("[+%5d] ", current->offset);
+        printf("[+%5d] ", current->offset);
         count++;
         if (count % 8 == 0){
             printf("\n");
         }
-	}
+    }
     printf("\n");
 }
 
 void printHeapMetaInfo()
-/**********************************************************
- * Print Heap Internal Bookkeeping Information
- *********************************************************/
+    /**********************************************************
+     * Print Heap Internal Bookkeeping Information
+     *********************************************************/
 {
     int i;
 
-	printf("\nHeap Meta Info:\n");
-	printf("===============\n");
-	printf("Total Size = %d bytes\n", hmi.totalSize);
-	printf("Start Address = %p\n", hmi.base);
+    printf("\nHeap Meta Info:\n");
+    printf("===============\n");
+    printf("Total Size = %d bytes\n", hmi.totalSize);
+    printf("Start Address = %p\n", hmi.base);
 
     for (i = hmi.maxIdx; i >=0; i--){
         printf("A[%d]: ", i);
@@ -136,16 +136,16 @@ void printHeapMetaInfo()
 }
 
 void printHeap()
-/**********************************************************
- * Print the content of the entire Heap 
- *********************************************************/
+    /**********************************************************
+     * Print the content of the entire Heap 
+     *********************************************************/
 {
     //Included as last debugging mechanism.
     //Print the entire heap regions as integer values.
 
     int* array;
     int size, i;
-    
+
     size = hmi.totalSize / sizeof(int);
     array = (int*)hmi.base;
 
@@ -161,9 +161,9 @@ void printHeap()
 }
 
 void printHeapStatistic()
-/**********************************************************
- * Print Heap Usage Statistics
- *********************************************************/
+    /**********************************************************
+     * Print Heap Usage Statistics
+     *********************************************************/
 {
     //TODO: Task 4. Calculate and report the various statistics
 
@@ -172,10 +172,10 @@ void printHeapStatistic()
 
     printf("Total Space: %d bytes\n", hmi.totalSize);
 
-   //Remember to preserve the message format!
+    //Remember to preserve the message format!
 
     printf("Total Space: %d bytes\n", hmi.totalSize);
-    
+
     printf("Total Free Partitions: %d\n", 0);
     printf("Total Free Size: %d bytes\n", 0);
 
@@ -183,26 +183,46 @@ void printHeapStatistic()
 }
 
 void addPartitionAtLevel( unsigned int lvl, unsigned int offset )
-/**********************************************************
- * There is just a suggested approach. You are free NOT to use this.
- *    This function adds a new free partition with "offset" at hmi.A[lvl]
- *    If buddy is found, recursively (or repeatedly) perform merging and insert
- *      at higher level
- *********************************************************/
+    /**********************************************************
+     *    This function adds a new free partition with "offset" at hmi.A[lvl]
+     *    If buddy is found, recursively (or repeatedly) perform merging and insert
+     *      at higher level
+     *********************************************************/
 {
-  
+    unsigned int buddyOffset = buddyOf(offset, lvl); 
+    partInfo* prev = NULL;
+
+    // look for free buddy and bubble up if possible:
+    for(partInfo* current = hmi.A[lvl]; current != NULL; prev = current, current = current->nextPart) { // linear search for buddy: 
+        if(current && current->offset == buddyOffset) { // found the buddy in the free list: 
+            // printf(">>> found a free buddy to merge with. Current level is %d and we shall merge tgt and add ourselves to level %d \n",lvl, lvl + 1);
+            unsigned int smallerOffsetValue = (current->offset < offset) ? current->offset : offset; // choose the smaller of the two
+            // remove current from this list:
+            if(!prev) {
+                hmi.A[lvl] = current->nextPart;
+            } else prev->nextPart = current->nextPart;
+            free(current);
+            return addPartitionAtLevel(lvl + 1, smallerOffsetValue);
+        }
+        // prev = current; 
+    }
+
+    // buddy is busy, add new partition to this level: 
+    partInfo* newPartition = buildPartitionInfo(offset); 
+    newPartition->nextPart = hmi.A[lvl];
+    hmi.A[lvl] = newPartition;
+    return;
 }
 
 partInfo* removePartitionAtLevel(unsigned int lvl)
-/**********************************************************
- * There is just a suggested approach. You are free NOT to use this.
- *    This function remove a free partition at hmi.A[lvl]
- *    Perform the "upstream" search if this lvl is empty AND perform
- *      the repeated split from higher level back to this level.
- * 
- * Return NULL if cannot find such partition (i.e. cannot sastify request)
- * Return the Partition Structure if found.
- *********************************************************/
+    /**********************************************************
+     *    This function removes a free partition at hmi.A[lvl]
+     *    Perform the "upstream" search if this lvl is empty AND perform
+     *    the repeated split from higher level back to this level.
+     * 
+     * Return NULL if cannot find such partition (i.e. cannot sastify request)
+     * Return the Partition Structure if found.
+     *********************************************************/
 {
     // level is the idx to look at
     partInfo* candidate = hmi.A[lvl];
@@ -212,49 +232,53 @@ partInfo* removePartitionAtLevel(unsigned int lvl)
             if(hmi.A[i] != NULL) {
                 largerTarget = hmi.A[i];
                 hmi.A[i] = largerTarget->nextPart;
-                // split into two partitions and add it to the front of the linked list:
-                int lvlToInsert = i - 1;
-                unsigned int selfOffset = largerTarget->offset;
-                unsigned int buddyOffset = buddyOf(selfOffset, lvlToInsert);
-                partInfo* self = buildPartitionInfo(selfOffset); 
+                // split larger into two buddy partitions and add it to the front of the linked list:
+                int newLevel = i - 1;
+                unsigned int selfOffset = largerTarget->offset; //
+                unsigned int buddyOffset = buddyOf(selfOffset, newLevel); // TODO: isn't buddyOf's param supp to be addr and not offset? 
+                partInfo* self = buildPartitionInfo(selfOffset); // unnecessary step, can just reasign ptrs
                 partInfo* buddy = buildPartitionInfo(buddyOffset);
                 self->nextPart = buddy;
-                buddy->nextPart = hmi.A[lvlToInsert];
-                hmi.A[lvlToInsert] = self;
+                buddy->nextPart = hmi.A[newLevel];
+                hmi.A[newLevel] = self;
                 break;
             }
         }
-        largerTarget == NULL ? NULL :  removePartitionAtLevel(lvl);
+        if(largerTarget == NULL) {
+            return NULL;
+        } else { 
+            free(largerTarget); 
+            return removePartitionAtLevel(lvl);
+        }
     } else {
-        // found an empty partition:
+        // found an empty, allocatable partition:
         hmi.A[lvl] = candidate->nextPart;
         return candidate;
     }
-
 }
 
 int setupHeap(int initialSize)
-/**********************************************************
- * Setup a heap with "initialSize" bytes
- *********************************************************/
+    /**********************************************************
+     * Setup a heap with "initialSize" bytes
+     *********************************************************/
 {
-	void* base;
+    void* base;
 
-	base = sbrk(0); // this helps (set and) find the current location of the program break
-	if(	sbrk(initialSize) == (void*)-1){
-		printf("Cannot set break! Behavior undefined!\n");
-		return 0;
-	}
+    base = sbrk(0); // this helps (set and) find the current location of the program break
+    if(	sbrk(initialSize) == (void*)-1){
+        printf("Cannot set break! Behavior undefined!\n");
+        return 0;
+    }
 
     hmi.base = base;
 
-	hmi.totalSize = initialSize;
+    hmi.totalSize = initialSize;
     hmi.internalFragTotal = 0;
-	
+
     //TODO: Task 1. Setup the rest of the bookkeeping info:
     //       hmi.A <= an array of partition linked list
     //       hmi.maxIdx <= the largest index for hmi.A[]
-    
+
     // Figure out the num of levels via continuous Log2:
     int numLevels = 0;
     while(initialSize != 0) {
@@ -264,7 +288,8 @@ int setupHeap(int initialSize)
 
     // allocate space for numLevels levels of partInfo arrays
     hmi.A = (partInfo**) malloc(sizeof(partInfo*) * numLevels);   //change this!
-    partInfo* initialFreePartition = (partInfo*) malloc(sizeof(partInfo));
+    partInfo* initialFreePartition = buildPartitionInfo(0);
+    // partInfo* initialFreePartition = (partInfo*) malloc(sizeof(partInfo));
     initialFreePartition->offset = 0;
     initialFreePartition->nextPart = (partInfo*) NULL;
     hmi.maxIdx = numLevels - 1; //change this!
@@ -277,24 +302,36 @@ int setupHeap(int initialSize)
 
 
 void* mymalloc(int size)
-/**********************************************************
- * Mimic the normal "malloc()":
- *    Attempt to allocate a piece of free heap of (size) bytes
- *    Return the memory addres of this free memory if successful
- *    Return NULL otherwise 
- *********************************************************/
+    /**********************************************************
+     * Mimic the normal "malloc()":
+     *    Attempt to allocate a piece of free heap of (size) bytes
+     *    Return the memory addres of this free memory if successful
+     *    Return NULL otherwise 
+     *********************************************************/
 {
     //TODO: Task 2. Implement the allocation using buddy allocator
-
+    printf(">>> mymalloc(%d)\n", size);
     partInfo* target = removePartitionAtLevel(log2Ceiling(size));
-    return (target == NULL)? NULL : hmi.base + target->offset;
+    void* memAddr = hmi.base + target->offset; 
+    if(!target) {
+        free(target);
+        return NULL;
+    } else {
+        free(target);
+        return memAddr;
+    }
 }
 
 void myfree(void* address, int size)
-/**********************************************************
- * Mimic the normal "free()":
- *    Attempt to free a previously allocated memory space
- *********************************************************/
+    /**********************************************************
+     * Mimic the normal "free()":
+     *    Attempt to free a previously allocated memory space
+     *********************************************************/
 {
     //TODO: Task 3. Implement the de allocation using buddy allocator
+    // int offset = address - hmi.base;
+    // printf(">>> myfree(%d,%d).\n", offset , size );
+
+    // add a partition to the hmi array to indicate it as free: 
+    addPartitionAtLevel(log2Ceiling(size), address - hmi.base);
 }
